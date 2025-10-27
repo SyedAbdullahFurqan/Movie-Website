821ff289b184dba957fafa3303ccc901442ac843
SyedAbdullahFurqan <megladon12515@gmail.com>
2025-10-27 22:44:50 +0500

    mobile fixed

--- src/Mobile.jsx
+++ src/Mobile.jsx
@@ -3,33 +3,38 @@ import { NavLink } from 'react-router-dom'
 
 const Mobile = () => {
   return (
-    <div className='bg-neutral-500 z-40 w-full fixed bottom-0 right-0'>
+    <div className='bg-neutral-800  z-40 w-full fixed bottom-0 right-0'>
       <nav className=' flex  items-center justify-center  '>
 
 
 
-<ul className='flex   py-2 md:items-center'>
+<ul className='flex  py-2 md:items-center'>
+    <li className='px-6'>
+        <NavLink to={"/"} style={({isActive})=>{
 
+return{color:isActive ?"red":"white",}
+        } } >Movie</NavLink>
+    </li>
     <li className='px-6'>
         <NavLink to={"/home"} style={({isActive})=>{
 
-return{color:isActive ?"red":"black",}
-        } } >Home</NavLink>
+return{color:isActive ?"red":"white",}
+        } } >All series</NavLink>
     </li>
 
     <li className='px-6'> 
         <NavLink to={"/About"}  style={({isActive})=>{
 
-return{color:isActive ?"red":"black",}
-        } }>About</NavLink>
+return{color:isActive ?"red":"white",}
+        } }>Tv Show</NavLink>
     
     </li>
   
     <li className='px-6'>
         <NavLink to={"/Contact"} style={({isActive})=>{
 
-return{color:isActive ?"red":"black",}
-        } }>Contact</NavLink>
+return{color:isActive ?"red":"white",}
+        } }>Movie</NavLink>
     </li>
 
 
